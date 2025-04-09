58dda8347bb9ccb4cae6b3eefde20729d3ace305
ArtemisShi <Artemiss2@nycstudents.net>
2025-04-09 13:11:59 +0000

    da

---
+++ src/main/java/com/example/project/test.java
@@ -0,0 +1,15 @@
+package com.example.project;
+
+
+public class test {
+    public static void main(String[] args) {
+       
+        System.out.println(RecursionChallenge.isPalindrome(""));
+        System.out.println(RecursionChallenge.isPalindrome("a"));
+        System.out.println(RecursionChallenge.isPalindrome("racecar"));
+        System.out.println(RecursionChallenge.isPalindrome("madam"));
+        System.out.println(RecursionChallenge.isPalindrome("hello"));
+        System.out.println(RecursionChallenge.isPalindrome("ab"));
+
+    }
+}
